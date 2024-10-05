c53d430775823f2a9fd11ae43ca492a82e492beb
Aarav <aarav25803691@gmail.com>
2024-10-05 18:26:06 -0500

    Add files via upload

---
+++ question7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <set> 
+using namespace std;
+int result(int *arr,int size){
+  arr.sort();
+  int result = arr[size];}
+int main(){
+  int arr[] = {90,9,99,39,29,39,29,29,29,292,292};
+  int arrSize = 9;
+  cout << result(arr, arrSize);
+}
